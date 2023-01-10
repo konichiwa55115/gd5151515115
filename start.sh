447e2dff494ddc7eb0f5543c6b89be6595891bbb
konichiwa55115 <121526988+konichiwa55115@users.noreply.github.com>
2023-01-10 08:08:45 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,6 @@
+echo "Cloning Repo...."
+git clone https://github.com/konichiwa55115/Google-Drive-Uploader-v2 /LazyDeveloper
+cd /LazyDeveloper
+pip3 install -r requirements.txt
+echo "Starting Bot...."
 python3 -m bot
